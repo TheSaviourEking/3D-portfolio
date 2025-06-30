7c5537e8be74142838c10dc684484962d822ae51
TheSaviourEking <saviour.eking@cyberservices.com>
2025-06-30 22:13:46 +0100

    Add index file to export section components for portfolio

---
+++ src/sections/index.jsx
@@ -0,0 +1,19 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import About from './About';
+import Projects from './Projects';
+import Experiences from './Experiences';
+import Testimonial from './Testimonial';
+import Contact from './Contact';
+import Footer from './Footer';
+
+export {
+    Navbar,
+    Hero,
+    About,
+    Projects,
+    Experiences,
+    Testimonial,
+    Contact,
+    Footer
+}
